eac8c514522c509d48e2624bc170b2f6d9ec3fcf
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2025-09-03 12:50:40 -0300

    HTML Body Margin 0 and load the background

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ const width = 1280;
 
 async function initPIXI() {
     app = new PIXI.Application();
-    const opcionesDePixi = { background: "#1099bb", width: width, height: height };
-    await app.init( opcionesDePixi );
+    const pixiOptions = { background: "#1099bb", width: width, height: height };
+    await app.init( pixiOptions );
 
     // Append the application canvas to the document body
     document.body.appendChild( app.canvas );
 
-    const texture = await PIXI.Assets.load("bunny.png");
+    // Load background
+    const bgTexture = await PIXI.Assets.load("stadium.png");
+    const background = new PIXI.Sprite(bgTexture);
+    background.x = 0;
+    background.y = 0;
+    background.width = width;
+    background.height = height;
+
+    // Load the background first (to be behind NPCs)
+    app.stage.addChild(background);
+
+    // Load the bunnies
+    const bunnyTexture = await PIXI.Assets.load("bunny.png");
 
     for ( let i = 0; i < 10; i++ ) {
-        const bunny = new PIXI.Sprite( texture );
+        const bunny = new PIXI.Sprite( bunnyTexture );
         bunny.x = Math.random() * width;
         bunny.y = Math.random() * height;
         bunnies.push ( bunny );
